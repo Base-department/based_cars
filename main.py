abd621c56abfb82e9cc0ab17018c2a2505df17f9
e24eeee <smyshlyaev1104@yandex.ru>
2022-09-16 22:34:42 +0300

    Бэк теперь работает как будто бы есть БД, но БД нет Т_Т

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import uvicorn
-from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import Tuple, Union
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
 
-
-class Car(BaseModel):
-    number: str
-    model: Union[str, None] = None
-    owner: Union[Tuple[int, int], None] = None
-    mileage: Union[int, None] = None
-
+from endpoints import crud
+from core.database import SessionLocal
+from schemas import CarSchema
 
 app = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/")
@@ -20,30 +23,42 @@ async def root():
 
 
 @app.get("/car/{id}")
-async def get_car(id: int):
-    return {"Car": id}
+async def get_car(id: int, db: Session = Depends(get_db)):
+    db_car = crud.get_car(db, car_id=id)
+    if db_car is None:
+        raise HTTPException(status_code=404, detail="NOT FOUND")
+    return db_car
 
 
 @app.get("/car")
-async def get_cars(page: int, per_page: int):
-    return {"All_cars": "Succ",
-            "Page_number": page,
-            "Cars_per_page": per_page}
+async def get_cars(page: int = 0, per_page: int = 10, db: Session = Depends(get_db)):
+    cars = crud.get_cars(db, page=page, per_page=per_page)
+    return cars
 
 
 @app.post("/car")
-async def post_car(car: Car):
-    return car
+async def post_car(car: CarSchema.CarCreate, db: Session = Depends(get_db)):
+    db_car = crud.get_car(db, car_id=car.id)
+    if db_car:
+        return crud.update_car(db, CarSchema.Car(id=car.id))
+    else:
+        return crud.create_car(db, car)
 
 
 @app.put("/car")
-async def update_car(car: Car):
-    return car
+async def update_car(car: CarSchema.Car, db: Session = Depends(get_db)):
+    db_car = crud.update_car(db, car)
+    if db_car is None:
+        raise HTTPException(status_code=404, detail="NOT FOUND")
+    return db_car
 
 
 @app.delete("/car")
-async def delete_car(car: Car):
-    return car
+async def delete_car(car: CarSchema.Car, db: Session = Depends(get_db)):
+    db_car = crud.delete_car(db, car)
+    if db_car is None:
+        raise HTTPException(status_code=404, detail="NOT FOUND")
+    return db_car
 
 
 if __name__ == "__main__":
